c7a88abe8e996cb1e461162914c7053245ae9fca
Brian Wisti <brianwisti@pobox.com>
2024-08-23 21:58:08 -0700

    pull assets from vault

--- build_scripts/pull.py
+++ build_scripts/pull.py
@@ -90,6 +90,31 @@ def has_descendants(note_path: Path, candidates: list[Path]) -> bool:
     return len(find_descendants(note_path, candidates)) > 0
 
 
+def pull_note(note: Note, link_for: dict[str, dict[str, str]]) -> None:
+    logging.info("Note: '%s'", note)
+    # replace Dendron-style links with Hugo-style links
+    content = note.post.content
+    for match in VAULT_LINK_RE.finditer(content):
+        link = match.group("fname")
+
+        if link in link_for:
+            title = link_for[link]["title"]
+            ref_for_link = "/" + link_for[link]["ref"]
+            partial = '{{< relref "' + ref_for_link + '" >}}'
+            content = content.replace(
+                match.group(0),
+                f"[{title}]({partial})",
+            )
+        else:
+            logging.warning("Link not found: %s", link)
+            content = content.replace(match.group(0), f"*{link}*")
+
+    note.post.content = content
+    content_path = Path(f"content/{note.content_path}")
+    content_path.parent.mkdir(parents=True, exist_ok=True)
+    content_path.write_text(frontmatter.dumps(note.post), encoding="utf-8")
+
+
 def main():
     logging.info("Initializing...")
     vault_path = Path("~/Documents/dendron-brain").expanduser()
@@ -104,28 +129,16 @@ def main():
     }
 
     for note in notes:
-        logging.info("Note: '%s'", note)
-        # replace Dendron-style links with Hugo-style links
-        content = note.post.content
-        for match in VAULT_LINK_RE.finditer(content):
-            link = match.group("fname")
+        pull_note(note, link_for)
 
-            if link in link_for:
-                title = link_for[link]["title"]
-                ref_for_link = "/" + link_for[link]["ref"]
-                partial = '{{< relref "' + ref_for_link + '" >}}'
-                content = content.replace(
-                    match.group(0),
-                    f"[{title}]({partial})",
-                )
-            else:
-                logging.warning("Link not found: %s", link)
-                content = content.replace(match.group(0), f"*{link}]*")
+    vault_assets_path = notes_path / "assets"
+    hugo_assets_path = Path("assets")
 
-        note.post.content = content
-        content_path = Path(f"content/{note.content_path}")
-        content_path.parent.mkdir(parents=True, exist_ok=True)
-        content_path.write_text(frontmatter.dumps(note.post), encoding="utf-8")
+    for asset in vault_assets_path.glob("**/*"):
+        if asset.is_file():
+            asset_path = hugo_assets_path / asset.relative_to(notes_path)
+            asset_path.parent.mkdir(parents=True, exist_ok=True)
+            asset_path.write_bytes(asset.read_bytes())
 
     logging.info("public notes: %s", len(public_note_paths))
     logging.info("Done!")
